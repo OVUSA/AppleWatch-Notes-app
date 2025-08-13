0c6740c9f2df213ff1f7d029d4162c0067d837e8
OVUSA <velichko.olga.chicago@gmail.com>
2025-08-12 23:00:05 -0500

    Added basic models - Note and User

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppleWatch_Notes_app.Models
+{
+    public class User
+    {
+        private string Name;
+        private string userId;
+        
+        IEnumerable<Note> personalNotes;
+
+    }
+}
