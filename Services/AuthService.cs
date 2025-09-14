51ad326c3ae7922aaced9a4bb415dfb7e97da904
OVUSA <velichko.olga.chicago@gmail.com>
2025-09-13 23:58:09 -0500

    Added Authentication

---
+++ Services/AuthService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace AppleWatch_Notes_app.Services
+{
+    public class AuthService
+    {
+    }
+    public string GenerateToken(int userId)
+    {
+       
+    }
+}
